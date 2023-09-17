f11ece37e8a41333baf27a119b68dcadbaed5340
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-09-17 14:26:31 +0530

    Create ZigZag Tree Traversal.cpp

---
+++ Trees/View-traversalofbinarytree/ZigZag Tree Traversal.cpp
@@ -0,0 +1,40 @@
+class Solution{
+    public:
+    //Function to store the zig zag order traversal of tree in a list.
+    vector <int> zigZagTraversal(Node* root)
+    {
+    	// Code here
+    	vector<int>result;
+    	if(root==NULL){
+    	    return result;
+    	}
+    	
+    	queue<Node*>q;
+    	q.push(root);
+    	bool ltor=true;
+    	while(!q.empty()){
+    	    int size=q.size();
+    	    vector<int>ans(size);
+    	    for(int i=0;i<size;i++){
+    	        
+    	        Node* temp=q.front();
+    	        q.pop();
+    	        int index=ltor?i:size-i-1;
+    	        ans[index]=temp->data;
+    	        if(temp->left){
+    	            q.push(temp->left);
+    	        }
+    	        if(temp->right){
+    	            q.push(temp->right);
+    	        }
+    	        
+    	    }
+    	    ltor=!ltor;
+    	    for(auto i:ans){
+    	        result.push_back(i);
+    	    }
+    	    
+    	}
+    	return result;
+    }
+};
