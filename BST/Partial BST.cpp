4850edf1171504c92f6bdbd3569e12ebdf07bc6d
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-09-22 18:47:44 +0530

    Create Partial BST.cpp

---
+++ BST/Partial BST.cpp
@@ -0,0 +1,15 @@
+bool solve(BinaryTreeNode<int> *root,int min,int max){
+    if(root==NULL){
+        return true;
+    }
+    if(root->data>=min && root->data<=max){
+        bool left=solve(root->left,min,root->data);
+        bool right=solve(root->right,root->data,max);
+        return left && right;
+    }
+    return false;
+}
+bool validateBST(BinaryTreeNode<int> *root) {
+    // Write your code here
+    return solve(root,INT_MIN,INT_MAX);
+}
