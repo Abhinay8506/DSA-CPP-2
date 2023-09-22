4bb292131f39f4cd708ff54122b099389e6877e0
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-09-22 18:51:27 +0530

    Create Preorder traversal of a BST.cpp

---
+++ BST/Preorder traversal of a BST.cpp
@@ -0,0 +1,19 @@
+BinaryTreeNode<int>* solve(vector<int> &preorder,int min,int max,int&i){
+    if(i>=preorder.size()){
+        return NULL;
+    }
+    if(preorder[i]<min || preorder[i]>max){
+        return NULL;
+    }
+    BinaryTreeNode<int>*node=new BinaryTreeNode<int>(preorder[i++]);
+    node->left=solve(preorder,min,node->data,i);
+    node->right=solve(preorder,node->data,max,i);
+    return node;
+}
+BinaryTreeNode<int>* preorderToBST(vector<int> &preorder) {
+    // Write your code here.
+    int min=INT_MIN;
+    int max=INT_MAX;
+    int i=0;
+    return solve(preorder,min,max,i);
+}
