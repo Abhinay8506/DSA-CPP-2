6cf62038faf2aad8972e2e92aa3b0d8acaa090aa
Abhinay Goswami <abhinay.21GCEBAIDS059@galgotiacollege.edu>
2023-09-24 21:09:06 +0530

    Create Convert BST to Min Heap.cpp

---
+++ Heaps/Convert BST to Min Heap.cpp
@@ -0,0 +1,45 @@
+void inorderTraversal(Node* root, vector<int>& arr)
+{
+    if (root == NULL)
+        return;
+ 
+
+    inorderTraversal(root->left, arr);
+ 
+   
+    arr.push_back(root->data);
+ 
+
+    inorderTraversal(root->right, arr);
+}
+ 
+// function to convert the given BST to MIN HEAP
+// performs preorder traversal of the tree
+void BSTToMinHeap(Node* root, vector<int> arr, int* i)
+{
+    if (root == NULL)
+        return;
+ 
+   
+    root->data = arr[++*i];
+ 
+
+    BSTToMinHeap(root->left, arr, i);
+ 
+ 
+    BSTToMinHeap(root->right, arr, i);
+}
+ 
+
+void convertToMinHeapUtil(Node* root)
+{
+    
+    vector<int> arr;
+    int i = -1;
+ 
+    
+    inorderTraversal(root, arr);
+ 
+
+    BSTToMinHeap(root, arr, &i);
+}
